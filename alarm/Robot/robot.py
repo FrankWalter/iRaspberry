b89fc42d402dcc96110a0ab41a08bf40c493154f
Ahriy <liuzhe314@qq.com>
2015-11-25 15:17:36 +0800

    add clock

--- alarm/Robot/robot.py
+++ alarm/Robot/robot.py
@@ -3,7 +3,7 @@ import main
 import random
 import thread
 import time
-import util.Voice
+import util.voice
 faceIndex = ('smile', 'smile2', 'angry', 'contempt', 'crafty', 'cute'\
              , 'love', 'shy', 'threaten')
 faces = {'smile':'smile', 'smile2': 'smile2', 'angry':'angry', \
@@ -14,73 +14,92 @@ weathers = {'sunny': 'sunny', 'cloudy': 'cloudy', 'overcast': 'overcast'\
             'heavy_rain': 'heavy_rain', 'moderate_snow': 'moderate_snow',\
             'heavy_snow': 'heavy_snow', 'light_snow': 'light_snow', 'fog': 'fog'}
 ISOTIMEFORMAT="%Y-%m-%d %X"
+
 class robot():
 
     def __init__(self, screen):
         self.screen = screen
+        self.number = []
+        self.screenHight = screen.get_height()
+        self.screenWidth = screen.get_width()
         self.initFace()
         self.initWeather()
+        self.loadNumberImg()
+
         self.faceOrWeather = 0 #decide what to display
                                # 0 stands for face and 1 stands for weather
         try :
-            thread.start_new_thread(self.displayWeather, ())
-            thread.start_new_thread(self.displayFace, ())
- #           thread.start_new_thread(self.mood, ())
+            thread.start_new_thread(self.display, ())
+            thread.start_new_thread(self.mood, ())
         except Exception as err:
             print err
 
+    def loadNumberImg(self):
+        for i in range(0,10):
+            path = '../resources/img/numbers/%i.png'%i
+            tmp = pygame.image.load(path).convert()
+            self.number.append(pygame.transform.scale(tmp, (self.screenWidth / 5, self.screenHight / 5)))
     def initFace(self):
         self.face = faces['smile']
         self.faceImg = pygame.image.load('../resources/img/' + self.face + '.png').convert()
-        self.faceImg = pygame.transform.scale(self.faceImg, (main.screenWidth, main.screenHight))
+        self.faceImg = pygame.transform.scale(self.faceImg, (self.screenWidth, self.screenHight))
 
     def initWeather(self):
         self.weather = weathers['fog']
         self.weatherImg = pygame.image.load('../resources/img/' + self.weather + '.png').convert()
-        self.weatherImg = pygame.transform.scale(self.weatherImg, (main.screenWidth, main.screenHight))
+        self.weatherImg = pygame.transform.scale(self.weatherImg, (self.screenWidth, self.screenHight))
 
     def changeFace(self, face):
         self.face = face
         self.faceImg = pygame.image.load('../resources/img/' + self.face + '.png').convert()
-        self.faceImg = pygame.transform.scale(self.faceImg, (main.screenWidth, main.screenHight))
+        self.faceImg = pygame.transform.scale(self.faceImg, (self.screenWidth, self.screenHight))
 
     def changeWeather(self, weather):
         self.weather = weather
         self.weatherImg = pygame.image.load('../resources/img/' + self.weather + '.png').convert()
-        self.weatherImg = pygame.transform.scale(self.weatherImg, (main.screenWidth, main.screenHight))
+        self.weatherImg = pygame.transform.scale(self.weatherImg, (self.screenWidth, self.screenHight))
 
-    def displayImg(self, img, location):
-        self.screen.blit(img,location)
-
-    def displayFace(self):
+    def display(self):
         location = [0, 0]
         while True:
             if self.faceOrWeather == 0:
-                self.displayImg(self.faceImg, location)
-                pygame.display.flip()
-
-    def displayWeather(self):
-        location = [0,0]
-        while True:
-            if self.faceOrWeather == 1:
-                self.displayImg(self.weatherImg, location)
+                self.screen.blit(self.faceImg, location)
+            elif self.faceOrWeather == 1:
+                self.screen.blit(self.weatherImg, location)
+            elif self.faceOrWeather == 2:
                 self.displayClock()
-                pygame.display.flip()
+            pygame.display.flip()
 
     def displayClock(self):
-        location = [0, main.screenHight / 2]
-        my_font = pygame.font.SysFont(None, main.screenWidth / 8)
-        textstr = time.strftime( ISOTIMEFORMAT, time.localtime() )
-        text_screen = my_font.render(textstr, True, (255, 0, 0))
-        self.displayImg(text_screen, location)
+        self.screen.fill([255, 255, 255])
+        stepHori = self.screenWidth / 20
+        stepVert = self.screenHight / 20
+        location1 = [0, stepVert * 5]
+        location2 = [stepHori * 5, stepVert * 5]
+        location31 = [stepHori / 4 * 40, stepVert * 6]
+        location32 = [stepHori / 4 * 40,  stepVert * 8]
+        location4 = [stepHori * 10, stepVert * 5]
+        location5 = [stepHori * 15, stepVert * 5]
+        now = time.localtime()
+        hour = now.tm_hour
+        minute = now.tm_min
+        second = now.tm_sec
+        self.screen.blit(self.number[hour / 10], location1)
+        self.screen.blit(self.number[hour % 10], location2)
+        self.screen.blit(self.number[minute / 10], location4)
+        self.screen.blit(self.number[minute % 10], location5)
+
+        if second % 2 == 0:
+            pygame.draw.circle(self.screen, [205, 0, 0], location31, self.screenWidth / 45)
+            pygame.draw.circle(self.screen, [205, 0, 0], location32, self.screenWidth / 45)
 
     def speak(self, sentence):
-        util.Voice.speak(sentence)
+        util.voice.speak(sentence)
 
     def mood(self):
         while True:
             time.sleep(2)
-            self.faceOrWeather = random.randint(0, 1)
+            self.faceOrWeather = (self.faceOrWeather + 1) % 3
 
 
 
--- alarm/main/main.py
+++ alarm/main/main.py
@@ -1,24 +1,24 @@
 # coding=utf8
 import pygame
+import os
+
+
 import sys
 sys.path.append('..')
 from pygame.locals import *
 from Robot.robot import *
 
-screenHight = 200
-screenWidth = 200
-def sleep_by_counter(threshold):
-    i = 0;
-    while i < threshold:
-        i += 1
-    return
+screenHight = 500
+screenWidth = 500
 
 def terminate():
     pygame.quit()
     sys.exit()
 def initScreen():
+    #force the screen to be displayed in full window
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
     screen = pygame.display.set_caption('Alarm Robot!')
-    screen = pygame.display.set_mode([screenWidth, screenHight], pygame.NOFRAME)
+    screen = pygame.display.set_mode((screenHight,screenWidth), pygame.NOFRAME)
     screen.fill([255,255,255])
     return screen
 if __name__=='__main__':
@@ -29,5 +29,5 @@ if __name__=='__main__':
         for event in pygame.event.get():
             if event.type == QUIT:
                 terminate()
-    rob.speak("肥料掺了金坷垃一带能顶两袋撒")
+    #rob.speak("肥料掺了金坷垃一带能顶两袋撒")
 
