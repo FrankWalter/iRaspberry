258cbb81b5256f962ef5601bcd126e084f841e04
Qi <qi@QideMacBook-Air.local>
2015-12-21 16:16:20 +0800

    weather function

---
+++ alarm/util/weather.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import sys, urllib, urllib2, json, time
+from pprint import pprint
+
+city = ""
+apikey = ""
+
+# input : 
+# output : string 
+# weather set : 
+
+def getWeather():
+	city = "shanghai"
+	apikey = "dda2c81ad20ca1e7b506c5696352a9e9"
+	url = 'http://apis.baidu.com/apistore/weatherservice/weather?citypinyin=' + city
+	req = urllib2.Request(url)
+	req.add_header("apikey", apikey)
+	resp = urllib2.urlopen(req)
+	oResp = json.loads(resp.read())
+	oRetData = oResp['retData']
+	weather = oRetData['weather'].encode('utf-8')
+	print weather
+	if "晴" in weather :
+		return "sunny" 
+	elif "雾" == weather:
+		return "fog"
+	elif "阴" == weather: 
+		return "overcast"
+	elif "雨" in  weather: 
+		return "rain"
+	elif "雪" in weather: 
+		return "snow"
+	else:
+		return "cloudy"
+	
+getWeather()
