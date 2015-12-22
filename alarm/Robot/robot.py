2ceb53e7a3225a99eb6dc7845e916e9e99157910
liuzhe <liuzhe314@qq.com>
2015-12-22 15:11:31 +0800

    counter thread proble

--- alarm/Robot/robot.py
+++ alarm/Robot/robot.py
@@ -4,16 +4,13 @@ import thread
 import time
 import util.voice
 import sys
-faceIndex = ('smile', 'smile2', 'angry', 'contempt', 'crafty', 'cute'\
+Faces = ('smile', 'smile2', 'angry', 'contempt', 'crafty', 'cute'\
              , 'love', 'shy', 'threaten')
-Faces = {'smile':'smile', 'smile2': 'smile2', 'angry':'angry', \
-         'contempt': 'contempt', 'crafty': 'crafty', 'cute': 'cute',\
-         'love': 'love', 'shy': 'shy', 'threaten': 'threaten'}
-Weathers = {'sunny': 'sunny', 'cloudy': 'cloudy', 'overcast': 'overcast'\
-    , 'light_rain': 'light_rain', 'moderate_rain': 'moderate_rain',\
-            'heavy_rain': 'heavy_rain', 'moderate_snow': 'moderate_snow',\
-            'heavy_snow': 'heavy_snow', 'light_snow': 'light_snow', 'fog': 'fog'}
+
+Weathers = ('sunny', 'cloudy', 'overcast', 'rain', 'snow', 'fog')
+
 Buttons = ("feed", "pet", "punish", "alarm", "music", "news",   "weather")
+
 ISOTIMEFORMAT="%Y-%m-%d %X"
 
 class robot():
@@ -22,27 +19,36 @@ class robot():
         self.screen = screen
         self.number = []
         self.buttons = []
-
+        self.weatherImg = []
+        self.facesImg = []
         self.screenHight = screen.get_height()
         self.screenWidth = screen.get_width()
-        self.initFace()
-        self.initWeather()
         self.buttonSize = self.screenWidth / 6
+
         self.loadImg()
 
-        self.Func = 0
-        #decide what action to perform
-        # 0 stands for feed
-        # 1 stands for pet
-        # 2 stands for punish
+        self.initFace()
+        self.initWeather()
+
+
+
         # 3 stands for alarm
         # 4 stands for music
         # 5 stands for news
         # 6 stands for weather
+        # 7 stands for just displaying face
+        self.Func = 7
+
+        # 0 stands for nothing
+        # 1 stands for feed
+        # 2 stands for pet
+        # 3 stands for punish
+        self.Treat = 0
 
         try :
-            thread.start_new_thread(self.performFunc, ())
-            thread.start_new_thread(self.bottonListener(), ())
+            thread.start_new_thread(self.bottonListener, ())
+            thread.start_new_thread(self.performTreatAndFunc, ())
+
         except Exception as err:
             print err
 
@@ -51,51 +57,51 @@ class robot():
             path = '../resources/img/numbers/%i.png'%i
             tmp = pygame.image.load(path).convert()
             self.number.append(pygame.transform.scale(tmp, (self.screenWidth / 5, self.screenHight / 5)))
+
         for i in range(0, len(Buttons)):
             path = '../resources/img/buttons/' + Buttons[i] + '.png'
             tmp = pygame.image.load(path).convert()
             array = []
             array.append(pygame.transform.scale(tmp, (self.buttonSize, self.buttonSize)))
             self.buttons.append(array)
+
+        for i in range(0, len(Faces)):
+            tmp = pygame.image.load('../resources/img/' + Faces[i] + '.png').convert()
+            tmp = pygame.transform.scale(tmp, (self.screenWidth, self.screenHight))
+            self.facesImg.append(tmp)
+
+        for i in range(0, len(Weathers)):
+            tmp = pygame.image.load('../resources/img/' + Weathers[i] + '.png').convert()
+            tmp = pygame.transform.scale(tmp, (self.screenWidth, self.screenHight))
+            self.weatherImg.append(tmp)
+
     def initFace(self):
-        self.face = Faces['smile']
-        self.faceImg = pygame.image.load('../resources/img/' + self.face + '.png').convert()
-        self.faceImg = pygame.transform.scale(self.faceImg, (self.screenWidth, self.screenHight))
+        self.faceImg = self.facesImg[0]
 
     def initWeather(self):
-        self.weather = Weathers['fog']
-        self.weatherImg = pygame.image.load('../resources/img/' + self.weather + '.png').convert()
-        self.weatherImg = pygame.transform.scale(self.weatherImg, (self.screenWidth, self.screenHight))
+        self.weatherImg = self.weatherImg[0]
 
-    def changeFace(self, face):
-        self.face = face
-        self.faceImg = pygame.image.load('../resources/img/' + self.face + '.png').convert()
-        self.faceImg = pygame.transform.scale(self.faceImg, (self.screenWidth, self.screenHight))
+    def changeFace(self, faceImg):
+        self.faceImg = faceImg
 
-    def changeWeather(self, weather):
-        self.weather = weather
-        self.weatherImg = pygame.image.load('../resources/img/' + self.weather + '.png').convert()
-        self.weatherImg = pygame.transform.scale(self.weatherImg, (self.screenWidth, self.screenHight))
+    def changeWeather(self, weatherImg):
+        self.weatherImg = weatherImg
 
-    def performFunc(self):
-        location = [0, 0]
+    def performTreatAndFunc(self):
         while True:
-            if self.Func == 0:
-                self.screen.blit(self.faceImg, location)
-                self.displayButtons()
+            if self.Treat == 1:
                 self.feedMe()
+                self.Treat = 0
 
-            elif self.Func == 1:
-                self.screen.blit(self.weatherImg, location)
-                self.displayButtons()
+            elif self.Treat == 2:
                 self.petMe()
+                self.Treat = 0
 
-            elif self.Func == 2:
-                self.screen.blit(self.weatherImg, location)
-                self.displayButtons()
+            elif self.Treat == 3:
                 self.punishMe()
+                self.Treat = 0
 
-            elif self.Func == 3:
+            if self.Func == 3:
                 self.displayClock()
 
             elif self.Func == 4:
@@ -106,6 +112,10 @@ class robot():
 
             elif self.Func == 6:
                 self.displayWeather()
+
+            elif self.Func == 7:
+                self.screen.blit(self.faceImg, [0, 0])
+                self.displayButtons()
 
             pygame.display.flip()
 
@@ -228,14 +238,19 @@ class robot():
     def bottonListener(self):
         while True:
             for event in pygame.event.get():
-                for i in range(0, len(self.buttons)):
-                    if self.Func == event.type == pygame.MOUSEBUTTONDOWN and self.buttons[i][1][0]<= event.pos[0] <= self.buttons[i][1][0] + self.buttonSize and self.buttons[i][1][1]<= event.pos[1] <= self.buttons[i][1][1] + self.buttonSize:
-                        print "click!%d" %i
-                        self.Func = i
-                    elif event.type == pygame.QUIT:
-                        print "quit!"
-                        pygame.quit()
-                        sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print "enter"
+                    # for i in range(0, len(self.buttons)):
+                    #     if self.buttons[i][1][0]<= event.pos[0] <= self.buttons[i][1][0] + self.buttonSize and self.buttons[i][1][1]<= event.pos[1] <= self.buttons[i][1][1] + self.buttonSize:
+                    #         if i >= 3:
+                    #             self.Func = i
+                    #         else:
+                    #             self.Treat = i + 1
+                    #             print self.Treat
+                elif event.type == pygame.QUIT:
+                    print "quit!"
+                    pygame.quit()
+                    sys.exit()
 
 
 
